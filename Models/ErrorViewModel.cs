bc818d8a417ef8599fa3802fb6fefd35f0654be2
Eric van Laar <eric.vanlaar@gmail.com>
2023-04-13 15:17:45 +0200

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MyfirstMVCApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
